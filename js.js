77513159c0a98e44a03e722dfcd7c5c5f0912729
srini88 <pvsrinivas.ece@gmail.com>
2016-04-23 23:57:35 -0500

    using ng-src

--- js.js
+++ js.js
@@ -3,14 +3,15 @@
 var myApp = angular.module("myModule", [])
 
 .controller('myController', ['$scope', function($scope){
-		var employee = {
+		
 
-			firstName : "Srini",
-			lastName : "vas",
-			gender : "LGBT"
-		};
+	var country = {
+		name : "AMURICA",
+		capital : "WICHITA, KS",
+		flag : "img.png"
+	};
+	$scope.country  = country;
 
-		$scope.employee = employee ;  //atching to scope..creating another employee on scope...
 	
 
  }]);
