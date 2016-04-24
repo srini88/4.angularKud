449c832aea9ab9e55460c020df957f40f1bd9ff1
srini88 <pvsrinivas.ece@gmail.com>
2016-04-24 17:31:49 -0500

    custom filter

--- js.js
+++ js.js
@@ -1,37 +1,34 @@
 
 
 var myApp = angular.module("myModule", [])
+.filter("gend", function(){
+
+    return function(gender){  //this gender is from employee.gender
+        console.log(gender);
+        if (gender ==1) return "male";
+        if (gender ==2) return "female";
+        if (gender ==3) return "kojja";
+    }
+
+})
 .controller('myController', ['$scope', function($scope){
 	
  var employees = [
-                { name: "Ben", gender: "Male", salary: 55000, city: "London" },
-                { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
-                { name: "Mark", gender: "Male", salary: 57000, city: "London" },
-                { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
-                { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+                { name: "Ben", gender: 1, salary: 55000, city: "London" },
+                { name: "Sara", gender: 2, salary: 68000, city: "Chennai" },
+                { name: "Mark", gender: 1, salary: 57000, city: "London" },
+                { name: "Pam", gender: 2, salary: 53000, city: "Chennai" },
+                { name: "Todd", gender: 3, salary: 60000, city: "London" },
             ];
 $scope.employees = employees;
-//this item passed from the ng-repeat stuff per employee
-$scope.search = function(item){
-
-    ///console.log(item); item will be passed inherently....
-    // <tr ng-repeat="employee in employees | filter:search">
- 
-    if ($scope.searchText === undefined){
-        return true;  ///true in the search filter...everything will be displayed..
-    }
-
-    else {
-                    if (item.city.toLowerCase()
-                                 .indexOf($scope.searchText.toLowerCase()) != -1 ||  //found
-                        item.name.toLowerCase()
-                                 .indexOf($scope.searchText.toLowerCase()) != -1) {
-                        return true;
-                    }
-                }
-                /// if you enter number or some shit like that...it wont search ..only searching through name and city
-    return false;
-}
 
 
  }]);
+
+// In this video we will discuss how to create a custom filter in AngularJS. 
+
+
+
+// Custom filter in AngularJS
+// 1. Is a function that returns a function
+// 2. Use the filter function to create a custom filter 
