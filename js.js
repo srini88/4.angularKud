f2f0541050a40201c8ac66c2ea1a7e5caed11ae7
srini88 <pvsrinivas.ece@gmail.com>
2016-04-23 23:13:39 -0500

    minified controller, complex obj

--- js.js
+++ js.js
@@ -1,26 +1,17 @@
-///creating a module....
 
-///first parameter..specifies the name...2nd parameter specifies the dependeicies of the module  []
 
-var myApp = angular.module("myModule", []);  ///here passing an empty array as a dependency..
+var myApp = angular.module("myModule", [])
 
-// in angular controller is a JS function..the job of the controller is to build a model for the view to display...
+.controller('myController', ['$scope', function($scope){
+		var employee = {
 
-// in real world,,controller may call from a web service that retrievs data from a DB...create a JS contructor function in Angular for a controller...
+			firstName : "Srini",
+			lastName : "vas",
+			gender : "LGBT"
+		};
 
-// you pass in parameter $scope...which is a object that is passed by angular automatically... you stick your data to your scope...  like attach message property... and make it available in the view..
+		$scope.employee = employee ;  //atching to scope..creating another employee on scope...
+	
 
-myApp.controller('myController', ['$scope', function($scope){
-	$scope.obj = {};
-	//function
-	$scope.obj.name = function(){
-		return "nigga";
-	}
-//property
-	$scope.obj.prop = "I'm a godammn property";
  }]);
 
-////you got to pass in scope in the array..as well as in the godammn function - very important from 1.5
-// myApp.controller('myController', ['$scope', function($scope){
-// 	$scope.message = "Angular JS tutorial";
-// }]);
