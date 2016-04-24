2e81d5083f3c1bb63d7ae6edd725876953fa2055
srini88 <pvsrinivas.ece@gmail.com>
2016-04-24 00:20:14 -0500

    printing using (key,value)

--- js.js
+++ js.js
@@ -3,13 +3,32 @@
 var myApp = angular.module("myModule", [])
 .controller('myController', ['$scope', function($scope){
 	//array of objects...
-	  var employees = [
-                    { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000 },
-                    { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000 },
-                    { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000 },
-                    { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000 },
-                    { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
+	  var countries = [
+                    {
+                        name: "UK",
+                        cities: [
+                            { name: "London" },
+                            { name: "Birmingham" },
+                            { name: "Manchester" }
+                        ]
+                    },
+                    {
+                        name: "USA",
+                        cities: [
+                            { name: "Los Angeles" },
+                            { name: "Chicago" },
+                            { name: "Houston" }
+                        ]
+                    },
+                    {
+                        name: "India",
+                        cities: [
+                            { name: "Hyderabad" },
+                            { name: "Chennai" },
+                            { name: "Mumbai" }
+                        ]
+                    }
                 ];
-	$scope.employees = employees;
+	$scope.countries = countries;
  }]);
 
