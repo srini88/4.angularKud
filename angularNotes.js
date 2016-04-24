d60797fdbf91225a8cf28b2a1e4818ad28372ce7
srini88 <pvsrinivas.ece@gmail.com>
2016-04-23 22:44:07 -0500

    initial commit sum of 2 nos

---
+++ angularNotes.js
@@ -0,0 +1,24 @@
+angular js framework 
+open source project 
+
+building spa's and line of business apps 
+
+many public facing websites built with angular...
+
+many JS frameworks to built web apps 
+
+- dependency injection---- 
+-- two way data binding... model and view in sync all times..
+-- testing in mind right from start....through UNit testing and end to end testing...
+
+---model view controller  (clean mvc way)split your code in MVC componenets...angular will manage it welll.....
+
+--controlling the behavior of DOM elemetns using directives...
+--- directives, filters, modules and routes....
+only one script file 
+
+ng-app  --- ng prefix stands for angular....ng-app is starting point of angular JS ...angular will check it in the HTMML page...if it is found, angular bootstraps itself and starts to manage section of the page...
+
+{{}} called binding expression
+
+
