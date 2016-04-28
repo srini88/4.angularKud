85b21dec7403246e8c7b2c844215ab28aa116da0
srini88 <pvsrinivas.ece@gmail.com>
2016-04-28 10:48:39 -0500

    routeparams got orig code back working

--- js.js
+++ js.js
@@ -1,11 +1,68 @@
+///ng-route itself is a godammn depenedycy..as we included another file..
 
-var app = angular.module("myApp",[])
-//removed all the scope shit and used this
-.controller("redController",function($scope, $rootScope){
-$scope.redColor = "Im a red color";
-$rootScope.rootScopeColor = "I'm a root scope color"
-})
-.controller("greenController",function($scope, $rootScope){
-$scope.greenColor = "Im a green color";
+//angular's config functio to specify configyration of our app
+//inject $routeProvider to configyre routes
+///routeProvider has got .when () function
+var myApp = angular.module("myModule", ['ngRoute'])
+//we are injecting routeProivder, also inject..locationProvider
+.config(function($routeProvider){
 
-});
+	$routeProvider
+	.when("/home", {  //if the user is on /home...use this view and ctlrs
+		templateUrl : "Templates/home.html",
+		controller   : "homeController"
+	})
+	.when("/courses", {
+		templateUrl : "Templates/courses.html",
+		controller : "coursesController"
+	})
+	.when("/students", {
+		templateUrl : "Templates/students.html",
+		controller : "studentsController"
+	})
+	.when("/students/:id", {   //very imp
+		templateUrl :"Templates/studentDetails.html",
+		controller :"studentDetailsController"
+
+	})
+	.otherwise({   //no first paramenter
+		redirectTo : "/home"   //automatically we go to /home..
+		//xyz -- again goes to /home..
+	})
+
+	
+}) //chain controller function
+.controller ("homeController", ['$scope', function($scope){
+	$scope.message = "Home Page";
+}])
+.controller ("coursesController", ['$scope', function($scope){
+	//return an array of courses..
+	$scope.courses = ["C#", "Angular", "JQuery", "ASP.NET"];
+}])
+.controller ("studentsController", ['$scope','$http', function($scope, $http){
+
+	////return a property callled students....has the web service connection.. using omdb webservice to get episodes..
+	$http.get('http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1')
+	.then(function (response){
+		$scope.students = response.data;
+		//console.log(response.data);
+	})
+}])
+.controller ("studentDetailsController", ['$routeParams','$scope', '$http', function($routeParams, $scope, $http){
+
+	$http({
+		url :"http://www.omdbapi.com/?",
+		params : {i:$routeParams.id},   //very very imp
+		method :"get"
+	})
+	.then(function (response){
+		$scope.student = response.data;  //this student is gonna contain that specific student details  (episode details)
+		console.log(response.data);
+	})
+
+}])
+
+
+// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=2
+// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1
+// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=3
