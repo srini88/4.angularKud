625122c337a20de1a81a371acd9b383d471a1eac
srini88 <pvsrinivas.ece@gmail.com>
2016-04-27 21:33:22 -0500

    traditional cotroller with scoep

--- js.js
+++ js.js
@@ -1,73 +1,7 @@
 
-///ng-route itself is a godammn depenedycy..as we included another file..
+var app = angular.module("myApp",[])
+.controller("myController",['$scope', function($scope){
 
-//angular's config functio to specify configyration of our app
-//inject $routeProvider to configyre routes
-///routeProvider has got .when () function
-var myApp = angular.module("myModule", ['ngRoute'])
-//we are injecting routeProivder, also inject..locationProvider
-.config(function($routeProvider){
-
-	$routeProvider
-	.when("/home", {  //if the user is on /home...use this view and ctlrs
-		templateUrl : "Templates/home.html",
-		controller   : "homeController"
-	})
-	.when("/courses", {
-		templateUrl : "Templates/courses.html",
-		controller : "coursesController"
-	})
-	.when("/students", {
-		templateUrl : "Templates/students.html",
-		controller : "studentsController"
-	})
-	.when("/students/:id", {   //very imp
-		templateUrl :"Templates/studentDetails.html",
-		controller :"studentDetailsController"
-
-	})
-	.otherwise({   //no first paramenter
-		redirectTo : "/home"   //automatically we go to /home..
-		//xyz -- again goes to /home..
-	})
-
-	
-}) //chain controller function
-.controller ("homeController", ['$scope', function($scope){
-	$scope.message = "Home Page";
-}])
-.controller ("coursesController", ['$scope', function($scope){
-	//return an array of courses..
-	$scope.courses = ["C#", "Angular", "JQuery", "ASP.NET"];
-}])
-.controller ("studentsController", ['$scope','$http', function($scope, $http){
-
-	////return a property callled students....has the web service connection.. using omdb webservice to get episodes..
-	$http.get('http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1')
-	.then(function (response){
-		$scope.students = response.data;
-		//console.log(response.data);
-	})
-}])
-.controller ("studentDetailsController", ['$routeParams','$scope', '$http', function($routeParams, $scope, $http){
-
-	$http({
-		url :"http://www.omdbapi.com/?",
-		params : {i:$routeParams.id},   //very very imp
-		method :"get"
-	})
-	.then(function (response){
-		$scope.student = response.data;  //this student is gonna contain that specific student details  (episode details)
-		console.log(response.data);
-	})
+$scope.msg= "nigga";
 
 }])
-
-
-// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=2
-// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1
-// http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=3
-	
-
-
-
